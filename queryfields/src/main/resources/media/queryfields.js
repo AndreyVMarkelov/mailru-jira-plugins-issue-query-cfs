03d857b0185e7a9aaed863598af3ce5830fef927
Andrey Markelov <a.markelov@corp.mail.ru>
2012-11-07 12:28:23 +0400

    fix IE bug

--- queryfields/src/main/resources/media/queryfields.js
+++ queryfields/src/main/resources/media/queryfields.js
@@ -1,12 +1,6 @@
 // Created by Andrey Markelov 29-08-2012.
 // Copyright Mail.Ru Group 2012. All rights reserved.
 
-jQuery(document).ready(function() {
-    jQuery(window).bind('beforeunload', function() {
-        return null;
-    });
-});
-
 //--> initialize dialog
 function initJqlDlg(baseUrl, cfId, prId, type)
 {
---
+++ queryfields/src/main/resources/media/queryfieldsadmin.js
@@ -0,0 +1,8 @@
+// Created by Andrey Markelov 29-08-2012.
+// Copyright Mail.Ru Group 2012. All rights reserved.
+
+jQuery(document).ready(function() {
+    jQuery(window).bind('beforeunload', function() {
+        
+    });
+});
