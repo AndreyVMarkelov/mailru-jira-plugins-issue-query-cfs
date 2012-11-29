c3c725819ff2c733bc84ff60be99a1f4d9e89e9d
Andrey Markelov <a.markelov@corp.mail.ru>
2012-11-29 17:23:20 +0400

    add linked validator

---
+++ queryfields/src/main/java/ru/mail/jira/plugins/lf/SingleLinkedFieldValidator.java
@@ -0,0 +1,103 @@
+/*
+ * Created by Andrey Markelov 29-11-2012.
+ * Copyright Mail.Ru Group 2012. All rights reserved.
+ */
+package ru.mail.jira.plugins.lf;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.StringTokenizer;
+import java.util.TreeSet;
+import com.atlassian.crowd.embedded.api.User;
+import com.atlassian.jira.ComponentManager;
+import com.atlassian.jira.bc.issue.search.SearchService;
+import com.atlassian.jira.component.ComponentAccessor;
+import com.atlassian.jira.issue.Issue;
+import com.atlassian.jira.issue.search.SearchException;
+import com.atlassian.jira.issue.search.SearchResults;
+import com.atlassian.jira.util.JiraWebUtils;
+import com.atlassian.jira.web.bean.PagerFilter;
+import com.atlassian.query.Query;
+import com.opensymphony.module.propertyset.PropertySet;
+import com.opensymphony.workflow.InvalidInputException;
+import com.opensymphony.workflow.Validator;
+import com.opensymphony.workflow.WorkflowException;
+
+/**
+ * Validate linked issues statuses.
+ *
+ * @author Andrey Markelov
+ */
+public class SingleLinkedFieldValidator
+    implements Validator
+{
+    /**
+     * Constructor.
+     */
+    public SingleLinkedFieldValidator() {}
+
+    @Override
+    public void validate(
+        Map transientVars,
+        Map args,
+        PropertySet ps)
+    throws InvalidInputException, WorkflowException
+    {
+        Issue issue = (Issue) transientVars.get("issue");
+
+        String jql = (String) args.get("jql");
+        String invalid_statuses = (String) args.get("invalid_statuses");
+
+        if (jql != null && jql.length() > 0)
+        {
+            if (jql.contains("RLINK"))
+            {
+                if (issue.getKey() == null)
+                {
+                    return;
+                }
+                jql = jql.replace("RLINK", issue.getKey());
+            }
+
+            User user = ComponentAccessor.getJiraAuthenticationContext().getLoggedInUser();
+            SearchService.ParseResult parseResult = ComponentManager.getInstance().getSearchService().parseQuery(user, jql);
+            if (parseResult.isValid())
+            {
+                Set<String> params = new TreeSet<String>();
+                if (invalid_statuses != null)
+                {
+                    StringTokenizer st = new StringTokenizer(invalid_statuses, "&");
+                    while (st.hasMoreTokens())
+                    {
+                        String token = st.nextToken();
+                        if (token.length() > 0)
+                        {
+                            params.add(token.trim());
+                        }
+                    }
+                }
+
+                Query query = parseResult.getQuery();
+                try
+                {
+                    SearchResults results = ComponentManager.getInstance().getSearchService().search(user, query, PagerFilter.getUnlimitedFilter());
+                    List<Issue> issues = results.getIssues();
+                    for (Issue i : issues)
+                    {
+                        if (params.contains(i.getStatusObject().getId()))
+                        {
+                            String issueUrl = Utils.getBaseUrl(JiraWebUtils.getHttpRequest()) + "/browse/" + i.getKey();
+                            throw new InvalidInputException(
+                                ComponentAccessor.getJiraAuthenticationContext().getI18nHelper().getText("queryfields.linkedvalidator.error", issueUrl, i.getStatusObject().getName()));
+                        }
+                    }
+                }
+                catch (SearchException e)
+                {
+                    //--> nothing to do
+                }
+            }
+        }
+    }
+}
---
+++ queryfields/src/main/java/ru/mail/jira/plugins/lf/SingleLinkedFieldValidatorFactory.java
@@ -0,0 +1,229 @@
+/*
+ * Created by Andrey Markelov 29-11-2012.
+ * Copyright Mail.Ru Group 2012. All rights reserved.
+ */
+package ru.mail.jira.plugins.lf;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.StringTokenizer;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import com.atlassian.jira.component.ComponentAccessor;
+import com.atlassian.jira.issue.fields.CustomField;
+import com.atlassian.jira.issue.status.Status;
+import com.atlassian.jira.plugin.workflow.AbstractWorkflowPluginFactory;
+import com.atlassian.jira.plugin.workflow.WorkflowPluginValidatorFactory;
+import com.opensymphony.workflow.loader.AbstractDescriptor;
+import com.opensymphony.workflow.loader.ValidatorDescriptor;
+
+/**
+ * Factory for validator that checks linked issues status.
+ *
+ * @author Andrey Markelov
+ */
+public class SingleLinkedFieldValidatorFactory
+    extends AbstractWorkflowPluginFactory
+    implements WorkflowPluginValidatorFactory
+{
+    /**
+     * Constructor.
+     */
+    public SingleLinkedFieldValidatorFactory() {}
+
+    /**
+     * Get linker fields.
+     */
+    private Map<String, String> getCustomFields()
+    {
+        Map<String, String> res = new TreeMap<String, String>();
+
+        List<CustomField> cgList = ComponentAccessor.getCustomFieldManager().getCustomFieldObjects();
+        for (CustomField cf : cgList)
+        {
+            if (cf.getCustomFieldType().getKey().equals("ru.mail.jira.plugins.lf.queryfields:mailru-linker-field"))
+            {
+                res.put(cf.getId(), cf.getName());
+            }
+        }
+
+        return res;
+    }
+
+    @Override
+    public Map<String, ?> getDescriptorParams(
+        Map<String, Object> validatorParams)
+    {
+        Map<String, Object> map = new HashMap<String, Object>();
+
+        if (validatorParams != null && validatorParams.containsKey("cfId"))
+        {
+            map.put("cfId", extractSingleParam(validatorParams, "cfId"));
+        }
+        else
+        {
+            map.put("cfId", "");
+        }
+
+        if (validatorParams != null && validatorParams.containsKey("jql"))
+        {
+            map.put("jql", extractSingleParam(validatorParams, "jql"));
+        }
+        else
+        {
+            map.put("jql", "");
+        }
+
+        if (validatorParams != null && validatorParams.containsKey("invalid_statuses"))
+        {
+            String[] statuses = (String[])validatorParams.get("invalid_statuses");
+            StringBuilder sb = new StringBuilder();
+            if (statuses != null)
+            {
+                for (String status : statuses)
+                {
+                    sb.append(status).append("&");
+                }
+            }
+            map.put("invalid_statuses", sb.toString());
+        }
+        else
+        {
+            map.put("invalid_statuses", "");
+        }
+
+        return map;
+    }
+
+    /**
+     * Get parameter from descriptor.
+     */
+    private String getParam(AbstractDescriptor descriptor, String param)
+    {
+        if (!(descriptor instanceof ValidatorDescriptor))
+        {
+            throw new IllegalArgumentException("Descriptor must be a ValidatorDescriptor.");
+        }
+
+        ValidatorDescriptor validatorDescriptor = (ValidatorDescriptor) descriptor;
+        String value = (String) validatorDescriptor.getArgs().get(param);
+
+        if (value!=null && value.trim().length() > 0)
+        {
+            return value;
+        }
+        else
+        {
+            return "";
+        }
+    }
+
+    /**
+     * Get collection parameters.
+     */
+    private Set<String> getSetParams(
+        AbstractDescriptor descriptor,
+        String param)
+    {
+        Set<String> params = new TreeSet<String>();
+
+        String paramStr = getParam(descriptor, param);
+        if (paramStr != null)
+        {
+            StringTokenizer st = new StringTokenizer(paramStr, "&");
+            while (st.hasMoreTokens())
+            {
+                String token = st.nextToken();
+                if (token.length() > 0)
+                {
+                    params.add(token.trim());
+                }
+            }
+        }
+
+        return params;
+    }
+
+    /**
+     * Get list of statuses.
+     */
+    private Map<String, String> getStatuses()
+    {
+        Map<String, String> res = new TreeMap<String, String>();
+
+        Collection<Status> statuses = ComponentAccessor.getConstantsManager().getStatusObjects();
+        if (statuses != null)
+        {
+            for (Status status : statuses)
+            {
+                res.put(status.getId(), status.getName());
+            }
+        }
+
+        return res;
+    }
+
+    @Override
+    protected void getVelocityParamsForEdit(
+        Map<String, Object> velocityParams,
+        AbstractDescriptor descriptor)
+    {
+        velocityParams.put("cfs", getCustomFields());
+        velocityParams.put("statuses", getStatuses());
+        velocityParams.put("cfId", getParam(descriptor, "cfId"));
+        velocityParams.put("jql", getParam(descriptor, "jql"));
+        velocityParams.put("invalid_statuses", getSetParams(descriptor, "invalid_statuses"));
+    }
+
+    @Override
+    protected void getVelocityParamsForInput(
+        Map<String, Object> velocityParams)
+    {
+        velocityParams.put("cfs", getCustomFields());
+        velocityParams.put("statuses", getStatuses());
+        velocityParams.put("cfId", "");
+        velocityParams.put("jql", "");
+        velocityParams.put("invalid_statuses", new TreeSet<String>());
+    }
+
+    @Override
+    protected void getVelocityParamsForView(
+        Map<String, Object> velocityParams,
+        AbstractDescriptor descriptor)
+    {
+        String cfId = getParam(descriptor, "cfId");
+        String realCf = "";
+        if (cfId != null && cfId.length() > 0)
+        {
+            CustomField cfObj = ComponentAccessor.getCustomFieldManager().getCustomFieldObject(cfId);
+            if (cfObj != null)
+            {
+                realCf = cfObj.getName();
+            }
+        }
+
+        StringBuilder realStatuses = new StringBuilder();
+        Set<String> statuses = getSetParams(descriptor, "invalid_statuses");
+        for (String status : statuses)
+        {
+            Status statusObj = ComponentAccessor.getConstantsManager().getStatusObject(status);
+            if (statusObj != null)
+            {
+                if (realStatuses.length() > 0)
+                {
+                    realStatuses.append(", ");
+                }
+                realStatuses.append(statusObj.getName());
+            }
+        }
+
+        velocityParams.put("cfId", getParam(descriptor, "cfId"));
+        velocityParams.put("realCf", realCf);
+        velocityParams.put("jql", getParam(descriptor, "jql"));
+        velocityParams.put("invalid_statuses", getSetParams(descriptor, "invalid_statuses"));
+        velocityParams.put("realStatuses", realStatuses.toString());
+    }
+}
